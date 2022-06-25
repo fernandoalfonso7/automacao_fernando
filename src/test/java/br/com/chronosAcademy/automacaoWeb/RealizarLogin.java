ebc3570437a141e8b86d9b6d82c52a78dd3227bb
fernandoalfonso7 <alfonso.fernando@hotmail.com>
2022-06-24 22:40:34 -0300

    anotacoes

---
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/RealizarLogin.java
@@ -0,0 +1,49 @@
+package br.com.chronosAcademy.automacaoWeb;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.*;
+
+public class RealizarLogin {
+
+    @Test
+    public void primeiroTeste() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver;
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://portal.app.foursys.com/");
+        String xpathTitulo1 = "//*[@id=\"root\"]/div[1]/div[1]/div/button";
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo1));
+        String titulo = txtTitulo.getText();
+        assertEquals("Entrar", titulo);
+        WebElement clickable = driver.findElement(By.xpath(xpathTitulo1));
+        new Actions(driver)
+                .click(clickable)
+                .perform();
+        String username = "fhortola";
+        String senha = "Zuz76449@";
+        String xpathcampouser = "//*[@id=\"user\"]";
+        String xpathcampokey = "//*[@id=\"password\"]";
+        driver.findElement(By.xpath(xpathcampouser)).sendKeys(username);
+        driver.findElement(By.xpath(xpathcampokey)).sendKeys(senha);
+        String xpathTitulo2 = "//*[@id=\"root\"]/div[1]/div/form/div/div/button";
+        WebElement clickable2 = driver.findElement(By.xpath(xpathTitulo2));
+        new Actions(driver)
+                .click(clickable2)
+                .perform();
+
+
+
+        driver.quit();
+    }
+
+}
--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -1,47 +1,47 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import static org.junit.Assert.*;
 
-public class Realizar_login {
+public class TesteWeb {
+
+    ChromeDriver driver;
+
+    @Before
+    public void inicializaTeste(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://portal.app.foursys.com");
+    }
 
     @Test
     public void primeiroTeste() {
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
-
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://portal.app.foursys.com/");
-        String xpathTitulo1 = "//*[@id=\"root\"]/div[1]/div[1]/div/button";
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo1));
+        String xpathTitulo = "//*[@id=\"root\"]/div[1]/div[1]/div/button";
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Entrar", titulo);
-        WebElement clickable = driver.findElement(By.xpath(xpathTitulo1));
-        new Actions(driver)
-                .click(clickable)
-                .perform();
-        String username = "fhortola";
-        String senha = "Zuz76449@";
-        String xpathcampouser = "//*[@id=\"user\"]";
-        String xpathcampokey = "//*[@id=\"password\"]";
-        driver.findElement(By.xpath(xpathcampouser)).sendKeys(username);
-        driver.findElement(By.xpath(xpathcampokey)).sendKeys(senha);
-        String xpathTitulo2 = "//*[@id=\"root\"]/div[1]/div/form/div/div/button";
-        WebElement clickable2 = driver.findElement(By.xpath(xpathTitulo2));
-        new Actions(driver)
-                .click(clickable2)
-                .perform();
 
-
-
-        //driver.quit();
+        driver.quit();
     }
 
+    @After
+    public void finalizaTeste(){
+        driver.quit();
+
+        }
+
+
 }
+
+
