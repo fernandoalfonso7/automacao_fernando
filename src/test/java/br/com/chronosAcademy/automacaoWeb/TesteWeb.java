36676b948b626dc1aa2f3ecf2e61f0f19d130caa
fernandoalfonso7 <alfonso.fernando@hotmail.com>
2022-06-24 22:35:00 -0300

    RealizLogin

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -1,32 +1,47 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 import static org.junit.Assert.*;
 
-public class TesteWeb {
+public class Realizar_login {
 
     @Test
-    public void primeiroTeste(){
+    public void primeiroTeste() {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver;
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.globo.com");
-        String xpathTitulo = "/html/body/section[2]/div/div/div[1]/div[1]/div/a/h3";
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
+        driver.get("https://portal.app.foursys.com/");
+        String xpathTitulo1 = "//*[@id=\"root\"]/div[1]/div[1]/div/button";
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo1));
         String titulo = txtTitulo.getText();
-        assertEquals("Governo decide elevar vale-gás e criar auxílio para caminhoneiros",titulo);
+        assertEquals("Entrar", titulo);
+        WebElement clickable = driver.findElement(By.xpath(xpathTitulo1));
+        new Actions(driver)
+                .click(clickable)
+                .perform();
+        String username = "fhortola";
+        String senha = "Zuz76449@";
+        String xpathcampouser = "//*[@id=\"user\"]";
+        String xpathcampokey = "//*[@id=\"password\"]";
+        driver.findElement(By.xpath(xpathcampouser)).sendKeys(username);
+        driver.findElement(By.xpath(xpathcampokey)).sendKeys(senha);
+        String xpathTitulo2 = "//*[@id=\"root\"]/div[1]/div/form/div/div/button";
+        WebElement clickable2 = driver.findElement(By.xpath(xpathTitulo2));
+        new Actions(driver)
+                .click(clickable2)
+                .perform();
 
-        driver.quit();
 
 
-
+        //driver.quit();
     }
 
 }
